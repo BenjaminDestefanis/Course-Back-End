971544f10c284efe1439289ba12c5ac7b76dc78d
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-10-01 12:46:11 -0300

    1/10/24 12:46 PM

---
+++ Exercises/Season-1/Ejercicios-2/index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+
+const PORT = 3000
+
+app.use(express.json())  // Middleware para interpretar JSON en el body de las solicitudes
+
+let tasks = []
+
+
+// Main Route
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public' ,'index.html')) // configuracion de la ruta del directorio
+})
+
+// Añadir tarea (POST)
+
+app.post('addTask', (req, res) => {
+    const {id, description } = req.body // Obtener info del cuerpo del documento
+    const newTask = { id, description, complete: false }; 
+    tasks.push(newTask)
+    res.status(201).json( { mensaje: 'Taks create with exit!', tarea: newTask} )
+})
+
+// Obtener todas las tareas (GET)
+
+app.get('/getTasks', (req, res) => {
+    res.json(tasks);
+})
+
+// Obtener una tarea (GET)
+
+app.get('/getTask/:id', (req, res) => {
+    const task = tasks.find(t => t.id === parseInt(req.params.id))
+    if (!task){
+        return res.status(404).json({ mensaje: 'Task not found'})
+    }
+
+    res.json(task)
+
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Server listen on PORT: ${PORT}`)
+})
