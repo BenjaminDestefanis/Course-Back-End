8c7403964f254d368a19cb97108745a72c7623cf
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-10-01 14:58:52 -0300

    1/10/24 2:58 PM

--- Exercises/Season-1/Ejercicios-2/index.js
+++ Exercises/Season-1/Ejercicios-2/index.js
@@ -5,31 +5,37 @@ const app = express()
 
 const PORT = 3000
 
-app.use(express.json())  // Middleware para interpretar JSON en el body de las solicitudes
+app.use(express.json())  // Middleware para interpretar JSON en el body de las solicitudes --
+app.use(express.static(path.join(__dirname, 'public'))) // Indica al servidor el uso de archivos estaticos --
+
 
 let tasks = []
 
 
-// Main Route
+// Main Route --
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname,'public' ,'index.html')) // configuracion de la ruta del directorio
-})
+}) */
 
-// Añadir tarea (POST)
+// Añadir tarea (POST) --
 
-app.post('addTask', (req, res) => {
+app.post('/addTask', (req, res) => {
+    console.log(req.body.description)
     const {id, description } = req.body // Obtener info del cuerpo del documento
     const newTask = { id, description, complete: false }; 
     tasks.push(newTask)
     res.status(201).json( { mensaje: 'Taks create with exit!', tarea: newTask} )
 })
 
-// Obtener todas las tareas (GET)
+// Obtener todas las tareas (GET) --
 
 app.get('/getTasks', (req, res) => {
     res.json(tasks);
 })
+
+
+
 
 // Obtener una tarea (GET)
 
@@ -56,6 +62,18 @@ app.put('/task/:id', (req, res) => {
     task.complete = complete !== undefined ? complete : task.complete
     
     res.json({ mensaje: 'Exit task Update!'})
+})
+
+// Eliminar una tarea (DELETE)
+
+app.delete('/task/:id', (req, res) => {
+    const idToEliminated = tasks.find(t => t.id === parseInt(req.params.id))
+    if(idToEliminated === -1){
+        return res.status(404).json({ mensaje: 'Task not found to delete ...'})
+    }
+
+    tasks.splice(idToEliminated, 1)
+    res.json({ mensaje: 'Task Eliminated !'})
 })
 
 
---
+++ Exercises/Season-1/Ejercicios-2/js.js
@@ -0,0 +1,39 @@
+const apiURL = 'http://localhost:3000/taks'
+
+document.getElementById('create-task-form').addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const id =          document.getElementById('task-id').value
+    const description = document.getElementById('task-description').value
+    const completed   = document.getElementById('task-completed')
+
+    fetch(apiURL, {
+        method:  'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ id: parseInt(id), description})
+    })
+
+    .then(response => response.json())
+    .then(data => {
+
+    })
+})
+
+
+
+const getTasks = () => {
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            const taskList = documet.getElementById('list-tasks')
+            taskList.innerHTML = ''
+
+            data.foreach(t => {
+                const li = document.createElement('li')
+                li.textContent = `ID: ${t.id}, Descripcion: ${t.description}, Completada: ${t.completed}`
+            })
+        })
+        
+}
