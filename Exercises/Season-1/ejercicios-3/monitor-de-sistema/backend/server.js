063f0646b61337019bfb8854daccf598846f0079
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-10-02 17:57:11 -0300

    10/02/24 5:57 PM

---
+++ Exercises/Season-1/ejercicios-3/monitor-de-sistema/backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const http = require("http")
+const socketIo = require("socket.io")
+const os = require("os")
+
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server)
+
+const PORT = process.env.PORT || 3400
+
+app.use(express.static('../frontend/public'))
+
+const getSystemData = () => {
+    const cpuLoad = os.loadavg()
+    const totalMemory = os.totalmem()
+    const freeMemory = os.freemem()
+    const diskUsage = 80
+
+    return {
+        cpuLoad: cpuLoad[0],
+        totalMemory,
+        freeMemory,
+        diskUsage
+    }
+}
+
+setInterval(() => {
+    io.emit('systemData', getSystemData())
+}, 2000)
+
+io.on('connection', (socket) => {
+    console.log('Cliente conectado')
+})
+
+server.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`))
